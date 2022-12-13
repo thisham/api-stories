9ee816b501d4468a1085f30af99a90e959b94308
Hamdan Yuwafi <yuwafi.hamdan365@gmail.com>
2022-12-13 18:01:27 +0700

    chore: setup env variables

--- next.config.js
+++ next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  env: {
+    DB_NAME: process.env.DB_NAME,
+    DB_HOST: process.env.DB_HOST,
+    DB_USERNAME: process.env.DB_USERNAME,
+    DB_PASSWORD: process.env.DB_PASSWORD,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
